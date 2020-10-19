4f874cd9723f091ec7c93af16dd80a1065400c3f
Alexmnzlms <alexmanza2@gmail.com>
2020-10-19 22:17:29 +0200

    Ejercicio 10

---
+++ sesion2/ejercicio10.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Ejercicio 9
+
+Partiendo de una disolución de ácido sulfúrico en agua al 80 % de concentración, quiero obtener
+una cantidad x de centímetros cúbicos a una concentración y% (y<80%). Siendo x, e y valores de
+entrada al programa, calcular cuantos centímetros cúbicos de la disolución al 80% y de agua son
+necesarios para obtener los x centímetros cúbicos deseados al y% de concentración.
+
+Autor: Alejandro Manzanares Lemus
+"""
+x = float(input('Centimetros cubicos: '))
+y = float(input('Concentracion (0, 100]: '))
+
+CONCENTRACION = 0.8
+
+# y = y / 100.0
+
+if y < CONCENTRACION:
+    agua = (0.8 * x * x) / y - x
+    print('Los cc de agua son:', agua)

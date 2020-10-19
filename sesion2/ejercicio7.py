f4343c14cb87f0419ce4e5f72299b3a055398602
Alexmnzlms <alexmanza2@gmail.com>
2020-10-19 19:37:39 +0200

    Ejercicio 7

---
+++ sesion2/ejercicio7.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Ejercicio 7
+
+Realizar un programa que pida el nombre de una persona, primer apellido, segundo apellido y
+que muestre por pantalla como sería el nombre completo en una sola línea. También mostrar el
+nombre completo pero al revés. Finalmente volver a descomponer el nombre completo en sus tres
+componentes y mostrarlos por pantalla.
+
+Autor: Alejandro Manzanares Lemus
+"""
+
+nombre = input('Nombre: ')
+apellido1 = input('Primer apellido: ')
+apellido2 = input('Segundo apellido: ')
+
+print('Nombre completo: ', nombre + ' ' + apellido1 + ' ' + apellido2)
+print('Nombre completo al revés: ', apellido2 + ' ' + apellido1 + ' ' + nombre)
+print('Nombre: ', nombre)
+print('Primer apellido: ', apellido1)
+print('Segundo apellido: ', apellido2)

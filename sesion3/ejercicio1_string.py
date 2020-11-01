434ed5e1d746dd79e49416801125d3728d722b42
Alexmnzlms <alexmanza2@gmail.com>
2020-11-01 18:57:27 +0100

    Ejercicio 2

--- sesion3/ejercicio1_string.py
+++ sesion3/ejercicio1_string.py
@@ -7,9 +7,11 @@ una letra en una palabra.
 
 Autor: Alejandro Manzanares Lemus
 """
-    
+
+def contar_letras(palabra, letra):
+    return palabra.count(letra)
 
 palabra = input('Introducza una palabra: ')
 letra = input('Introduza una letra: ')
 
-print('El numero de', letra, 'de', palabra, 'es', palabra.count(letra))
+print('El numero de', letra, 'de', palabra, 'es', contar_letras(palabra, letra))
---
+++ sesion3/ejerciciox.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Ejercicio 
+    
+
+
+Autor: Alejandro Manzanares Lemus
+"""

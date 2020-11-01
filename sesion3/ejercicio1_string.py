e2024f88bba74e373ee2d411a4652bb9cd2f9086
Alexmnzlms <alexmanza2@gmail.com>
2020-11-01 18:48:27 +0100

    Ejercicio 1

---
+++ sesion3/ejercicio1_string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Ejercicio 1
+    
+Escribe una función contar_letras(palabra, letra) que devuelva el número de veces que aparece
+una letra en una palabra.
+
+Autor: Alejandro Manzanares Lemus
+"""
+    
+
+palabra = input('Introducza una palabra: ')
+letra = input('Introduza una letra: ')
+
+print('El numero de', letra, 'de', palabra, 'es', palabra.count(letra))

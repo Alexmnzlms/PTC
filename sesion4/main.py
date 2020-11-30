dc156a1b6b99b60cefc3c206b90cb23a6d8abe80
Alexmnzlms <alexmanza2@gmail.com>
2020-11-30 19:45:24 +0100

    Ejercicio especial

--- sesion4/main.py
+++ sesion4/main.py
@@ -1,9 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 30 12:10:02 2020
+Created on Mon Nov 30 19:39:45 2020
 
 @author: Alejandro Manzanares Lemus
 """
+
+# EJERCICIO DE COMPENSIÓN DE LISTAS
+
+# Método tradicional
+
+lista = []
+for numero in range(1,11):
+    lista.append(numero**2)
+    
+pares = []
+for numero in lista:
+    if numero % 2 == 0:
+        pares.append(numero)
+        
+# Versión compresion de lista        
+
+pares_comp = [x**2 for x in range(1,11) if x**2 % 2 == 0 ]
+
+print('Método tradicional', pares)
+print('Compresión de listas', pares_comp)
+
+# EJERCICIO DE ELIMINACIÓN DE VALOR EN LISTA DE 6 FORMAS
 
 def elim1(lista, n):
     while lista.count(n) > 0:
